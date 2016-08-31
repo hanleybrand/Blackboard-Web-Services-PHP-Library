1efdc544c5f3d912609572e5b7fe03f8336d7cfc
Peter Hanley <hanley@Peter2014.tu.temple.edu>
2016-08-31 18:35:02 -0400

    depersonalized the namespace, added $sent & $rec for debugging handiness
    now use
    
    ```php
    $blackboard = new \bbws\BbPhp($server, $use_curl);
    ```
    to make a new instance, and you can use something like
    
    ```php
        $sent = $blackboard->sent;
        echo "<pre>" . print_r($sent[count($sent)-1], true) . "\n" .
    "</pre>";
    ```
    
    to check the most recent thing you $sent

--- src/bbphp.php
+++ src/bbphp.php
@@ -1,61 +1,66 @@
-<?php namespace kreeves;
-/*
- * BbPHP: Blackboard Web Services Library for PHP
- * Copyright (C) 2011 by St. Edward's University (www.stedwards.edu)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- */ 
- 
-include('services/service.php'); 
-include('services/announcement.php');
-include('services/calendar.php');
-include('services/content.php');
-include('services/context.php');
-include('services/course.php');
-include('services/coursemembership.php');
-include('services/gradebook.php');
-include('services/user.php');
-include('services/util.php');
+<?php namespace bbws;
+	/*
+     * BbPHP: Blackboard Web Services Library for PHP
+     * Copyright (C) 2011 by St. Edward's University (www.stedwards.edu)
+     *
+     * Licensed under The MIT License
+     * Redistributions of files must retain the above copyright notice.
+     */
 
-class BbPhp {
-	
-	private $session_id = null;
-	private $services = array('Announcement', 'Calendar', 'Content', 'Context', 'Course', 'CourseMembership', 'Gradebook', 'NotificationDistributorOperations', 'User', 'Util');
-	public $url = null;
-	public $use_curl = true;
-	
-	public function __construct($url = null, $use_curl = true) {
-		$this->url = $url;
-		$this->use_curl = $use_curl;
-		$this->session_id = $this->Context("initialize");
-	}
+	include('services/service.php');
+	include('services/announcement.php');
+	include('services/calendar.php');
+	include('services/content.php');
+	include('services/context.php');
+	include('services/course.php');
+	include('services/coursemembership.php');
+	include('services/gradebook.php');
+	include('services/user.php');
+	include('services/util.php');
 
-    public function getSessionId()
-    {
-        return $this->session_id;
-    }
+	class BbPhp {
 
-    public function setSessionId($session)
-    {
-        $this->session_id = $session;
-    }
-	
-	private function buildHeader() {
-		$stamp = \gmdate('Y-m-d\TH:i:s\Z', time());
+		private $session_id = null;
+		private $services = array('Announcement', 'Calendar', 'Content', 'Context', 'Course', 'CourseMembership', 'Gradebook', 'NotificationDistributorOperations', 'User', 'Util');
+		public $url = null;
+		public $use_curl = true;
+		public $sent = null;
+		public $rec = null;
 
-		if ($this->session_id == null) {
-			$password = 'nosession';
-		} else {
-			$password = $this->session_id;
+		public function __construct($url = null, $use_curl = true) {
+			$this->url = $url;
+			$this->use_curl = $use_curl;
+			$this->session_id = $this->Context("initialize");
+			$this->sent = array();
+			$this->rec = array();
 		}
-		
-		/**
-		 * This header is sensitive to line breaks and you should avoid
-		 * letting things like Eclipse do auto-formatting on it.    
-		 * 
-		 **/
-		$header = <<<END
+
+		public function getSessionId()
+		{
+			return $this->session_id;
+		}
+
+		public function setSessionId($session)
+		{
+			$this->session_id = $session;
+		}
+
+
+		private function buildHeader() {
+			$stamp = \gmdate('Y-m-d\TH:i:s\Z', time());
+
+			if ($this->session_id == null) {
+				$password = 'nosession';
+			} else {
+				$password = $this->session_id;
+			}
+
+			/**
+			 * This header is sensitive to line breaks and you should avoid
+			 * letting things like Eclipse do auto-formatting on it.
+			 *
+			 **/
+			$header = <<<END
 		<SOAP-ENV:Header>
 	        <wsse:Security SOAP-ENV:mustunderstand="true" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
 	            <wsse:Timestamp xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd">
@@ -68,148 +73,149 @@ class BbPhp {
 	        </wsse:Security>
 	    </SOAP-ENV:Header>
 END;
-		return $header;
-	}
-	
-	private function buildRequest($method = null, $service, $args = null) {
-		$header = $this->buildHeader();
-		
-		$serviceObject = new $service();
-
-		$body = $serviceObject->$method($args);
-
-		$request = '<SOAP-ENV:Envelope xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
-		$request .= $header.$body;
-		$request .= '</SOAP-ENV:Envelope>';
-
-		return $request;
-	}
-	
-	/**
-	 * The call() magic method is used here to access methods from BB that have not been defined in the class.
-	 * In theory you can access any method that the BB object provides if you have added it to your "Context". 
-	 */
-	public function __call($method, $args) {
-		if (!isset($args[1])) {
-			$args[1] = null;
+			return $header;
 		}
 
-		if (\in_array($method, $this->services)) {
-			return $this->doCall($args[0], $method, $args[1]);
-		} else {
-			return $this->doCall($method, $args[0], $args[1]);
-		}
-	}
-	
-	public function doCall($method = null, $service = "Context", $args = null) {
-		$request = $this->buildRequest($method, $service, $args);
+		private function buildRequest($method = null, $service, $args = null) {
+			$header = $this->buildHeader();
 
-		if ($this->use_curl) {
-			$ch = \curl_init();
-				
-			\curl_setopt($ch, CURLOPT_URL, $this->url . '/webapps/ws/services/' . $service . '.WS');
-			\curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml; charset=utf-8', 'SOAPAction: "' . $method . '"'));
-			\curl_setopt($ch, CURLOPT_POST, 1);
-			\curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
-			\curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			
-			$result = \curl_exec($ch);
+			$serviceObject = new $service();
 
-			\curl_close($ch);
-		} else {
-			$result = $this->doPostRequest($this->url . '/webapps/ws/services/' . $service . '.WS', $request, "Content-type: text/xml; charset=utf-8\nSOAPAction: \"" . $method . "\"");
+			$body = $serviceObject->$method($args);
+
+			$request = '<SOAP-ENV:Envelope xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance">';
+			$request .= $header.$body;
+			$request .= '</SOAP-ENV:Envelope>';
+
+			return $request;
 		}
 
-		$result_array = $this->xmlstr_to_array($result);
-        if ($method == "loginTool") {
-            $final_result = (isset($result_array['Body'][$method . 'Response']['return'])) ? $result_array['Body'][$method . 'Response']['return'] : $result_array;
-        } else {
-            $final_result = (isset($result_array['Body'][$method . 'Response']['return'])) ? $result_array['Body'][$method . 'Response']['return'] : null;
-        }
+		/**
+		 * The call() magic method is used here to access methods from BB that have not been defined in the class.
+		 * In theory you can access any method that the BB object provides if you have added it to your "Context".
+		 */
+		public function __call($method, $args) {
+			if (!isset($args[1])) {
+				$args[1] = null;
+			}
 
-
-		return $final_result;
-	}	
-	
-	/*
-	 * This function can be found here:
-	 * http://wezfurlong.org/blog/2006/nov/http-post-from-php-without-curl/
-	 */
-	private function doPostRequest($url, $data, $optional_headers = null) {
-		$params = array('http' => array(
-		            'method' => 'POST',
-		            'content' => $data
-		          ));
-		if ($optional_headers !== null) {
-			$params['http']['header'] = $optional_headers;
+			if (\in_array($method, $this->services)) {
+				return $this->doCall($args[0], $method, $args[1]);
+			} else {
+				return $this->doCall($method, $args[0], $args[1]);
+			}
 		}
-		$ctx = \stream_context_create($params);
-		$fp = @fopen($url, 'rb', false, $ctx);
-		if (!$fp) {
-			throw new \Exception("Problem with $url, $php_errormsg");
-		}
-		$response = @stream_get_contents($fp);
-		if ($response === false) {
-			throw new \Exception("Problem reading data from $url, $php_errormsg");
-		}
-		return $response;
-	}	
 
-	function xmlstr_to_array($xmlstr) {
-		$doc = new \DOMDocument();
-		$doc->loadXML($xmlstr);
-		return $this->domnode_to_array($doc->documentElement);
-	}
-	
-	function domnode_to_array($node) {
-		$output = array();
-		
-		switch ($node->nodeType) {
-			case XML_CDATA_SECTION_NODE:
-			case XML_TEXT_NODE:
-				$output = trim($node->textContent);
-				break;
-			case XML_ELEMENT_NODE:
-				for ($i=0, $m=$node->childNodes->length; $i<$m; $i++) {
-					$child = $node->childNodes->item($i);
-					$v = $this->domnode_to_array($child);
-					if(isset($child->tagName)) {
-						$t = $child->tagName;
-						if (\strpos($t, ':') !== false) {
-							$temp = explode(':', $t);
-							$key = $temp[1];
-						} else {
-							$key = $t;
-						}
-						$test = substr($t, strpos($t, ':') + 1, strlen($t) - strpos($t, ':') + 1);
-						if(!isset($output[$key])) {
-							$output[$key] = array();
-						}
-						$output[$key][] = $v;
-					} elseif($v) {
-						$output = (string) $v;
-					}
-				}	
+		public function doCall($method = null, $service = "Context", $args = null) {
+			$request = $this->buildRequest($method, $service, $args);
 
-				if(\is_array($output)) {
-					if($node->attributes->length) {
-						$a = array();
-						foreach($node->attributes as $attrName => $attrNode) {
-							$a[$attrName] = (string) $attrNode->value;
-						}
-						$output['@attributes'] = $a;
-					}
-					foreach ($output as $t => $v) {
-						if(\is_array($v) && \count($v)==1 && $t!='@attributes') {
-							$output[$t] = $v[0];
+			if ($this->use_curl) {
+				$ch = \curl_init();
+
+				\curl_setopt($ch, CURLOPT_URL, $this->url . '/webapps/ws/services/' . $service . '.WS');
+				\curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-type: text/xml; charset=utf-8', 'SOAPAction: "' . $method . '"'));
+				\curl_setopt($ch, CURLOPT_POST, 1);
+				\curl_setopt($ch, CURLOPT_POSTFIELDS, $request);
+				\curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+				$result = \curl_exec($ch);
+
+				\curl_close($ch);
+			} else {
+				$result = $this->doPostRequest($this->url . '/webapps/ws/services/' . $service . '.WS', $request, "Content-type: text/xml; charset=utf-8\nSOAPAction: \"" . $method . "\"");
+			}
+
+			$result_array = $this->xmlstr_to_array($result);
+			if ($method == "loginTool") {
+				$final_result = (isset($result_array['Body'][$method . 'Response']['return'])) ? $result_array['Body'][$method . 'Response']['return'] : $result_array;
+			} else {
+				$final_result = (isset($result_array['Body'][$method . 'Response']['return'])) ? $result_array['Body'][$method . 'Response']['return'] : null;
+			}
+			$this->sent[] = $request;
+			$this->rec[] = $result;
+
+			return $final_result;
+		}
+
+		/*
+         * This function can be found here:
+         * http://wezfurlong.org/blog/2006/nov/http-post-from-php-without-curl/
+         */
+		private function doPostRequest($url, $data, $optional_headers = null) {
+			$params = array('http' => array(
+				'method' => 'POST',
+				'content' => $data
+			));
+			if ($optional_headers !== null) {
+				$params['http']['header'] = $optional_headers;
+			}
+			$ctx = \stream_context_create($params);
+			$fp = @fopen($url, 'rb', false, $ctx);
+			if (!$fp) {
+				throw new \Exception("Problem with $url, $php_errormsg");
+			}
+			$response = @stream_get_contents($fp);
+			if ($response === false) {
+				throw new \Exception("Problem reading data from $url, $php_errormsg");
+			}
+			return $response;
+		}
+
+		function xmlstr_to_array($xmlstr) {
+			$doc = new \DOMDocument();
+			$doc->loadXML($xmlstr);
+			return $this->domnode_to_array($doc->documentElement);
+		}
+
+		function domnode_to_array($node) {
+			$output = array();
+
+			switch ($node->nodeType) {
+				case XML_CDATA_SECTION_NODE:
+				case XML_TEXT_NODE:
+					$output = trim($node->textContent);
+					break;
+				case XML_ELEMENT_NODE:
+					for ($i=0, $m=$node->childNodes->length; $i<$m; $i++) {
+						$child = $node->childNodes->item($i);
+						$v = $this->domnode_to_array($child);
+						if(isset($child->tagName)) {
+							$t = $child->tagName;
+							if (\strpos($t, ':') !== false) {
+								$temp = explode(':', $t);
+								$key = $temp[1];
+							} else {
+								$key = $t;
+							}
+							$test = substr($t, strpos($t, ':') + 1, strlen($t) - strpos($t, ':') + 1);
+							if(!isset($output[$key])) {
+								$output[$key] = array();
+							}
+							$output[$key][] = $v;
+						} elseif($v) {
+							$output = (string) $v;
 						}
 					}
-				}
-			break;
+
+					if(\is_array($output)) {
+						if($node->attributes->length) {
+							$a = array();
+							foreach($node->attributes as $attrName => $attrNode) {
+								$a[$attrName] = (string) $attrNode->value;
+							}
+							$output['@attributes'] = $a;
+						}
+						foreach ($output as $t => $v) {
+							if(\is_array($v) && \count($v)==1 && $t!='@attributes') {
+								$output[$t] = $v[0];
+							}
+						}
+					}
+					break;
+			}
+
+			return $output;
 		}
-		
-		return $output;
-	}	
-	
-}
+
+	}
 ?>
